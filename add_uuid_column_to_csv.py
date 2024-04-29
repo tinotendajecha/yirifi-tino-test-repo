db4524e6866ffc41741a1da61cc5c7e5465c6182
tino-yirifi <168447398+tino-yirifi@users.noreply.github.com>
2024-04-29 16:08:54 +0200

    Add files via upload

---
+++ add_uuid_column_to_csv.py
@@ -0,0 +1,13 @@
+import uuid
+import pandas as pd
+
+
+
+
+def add_uuid_to_article(data_frame):
+    data_frame['uuid'] = [uuid.uuid4() for _ in range(len(data_frame.index))]
+    return data_frame 
+
+data_frame = pd.read_csv('post-sitemap-1.csv')
+
+add_uuid_to_article(data_frame).to_csv('post-sitemap-1.csv', index=False)
